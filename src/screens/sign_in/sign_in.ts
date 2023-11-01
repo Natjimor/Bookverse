c071c80872cf1ee36888424982d0ae11b10b8aed
valen-web <128190072+valen-web@users.noreply.github.com>
2023-11-01 14:09:07 -0500

    sing_in

---
+++ src/screens/sign_in/sign_in.ts
@@ -0,0 +1,33 @@
+import { inputs } from "../../components/inputs/inputs";
+import { Welcome } from "../../components/welcome/welcome";
+import { button } from "../../components/button_continue/continue";
+
+export class sing_in extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="/dist/sign_in/css/component.css">
+        <section>
+        <div id="back">
+        <div id="white">
+        <welcome-container></welcome-container>
+        <inputs-container></inputs-container>
+        <button-container></button-container>
+        </div>
+        </div>
+        </section>
+        `;
+    }
+  }
+}
+
+customElements.define(`sing-container`, sing_in);
