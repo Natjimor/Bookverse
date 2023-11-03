b01f8df3aaabfe1d35a4f144b22658ad5a85b6a3
Natjimor <125272271+Natjimor@users.noreply.github.com>
2023-11-03 18:23:48 -0500

    Delete src directory

--- src/store/acctions.ts
+++
@@ -1,9 +0,0 @@
-import { Screens } from "../types/navigation";
-import { Actions } from "../types/store";
-
-export const navigate = (screen: Screens) => {
-    return {
-        type: "NAVIGATE",
-        payload: screen,
-    };
-};
