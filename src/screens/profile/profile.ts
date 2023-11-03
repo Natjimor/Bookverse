86f21dd1715f25e714889213c155d29e4b27aae2
valen-web <128190072+valen-web@users.noreply.github.com>
2023-11-03 10:59:57 -0500

    Add files via upload

--- src/screens/profile/profile.ts
+++ src/screens/profile/profile.ts
@@ -1,7 +1,8 @@
-import { nav } from "../../components/search_bar/bar";
-import { photo } from "../../components/photo/photo";
+import { nav, photo, bar_library, library } from "../../export";
+import { menu } from "../../components/search_bar/nav_bar/profile/menu";
+console.log(menu);
 console.log(photo);
-
+console.log(bar_library, library);
 
 export class porfile extends HTMLElement {
     constructor() {
@@ -19,7 +20,10 @@ export class porfile extends HTMLElement {
         
         this.shadowRoot.innerHTML = `
          <nav-container></nav-container>
+         <menu-container></menu-container>
          <photo-container></photo-container>
+         <barlib-container></barlib-container>
+         <library-container></library-container>
         `
     }
 }
