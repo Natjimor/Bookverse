e39750f34f1596886df1298b9d152363b6175df8
Natjimor <125272271+Natjimor@users.noreply.github.com>
2023-11-03 16:52:26 -0500

    export

--- src/components/export.ts
+++ src/components/export.ts
@@ -5,25 +5,8 @@ export {default as landingDiscover} from "./landing/landing_discover/discover"
 export {default as landingFavorite} from "./landing/landing_favorite/favorite"
 export {default as landingQuote} from "./landing/landing_quote/quote"
 export {default as landingInfo} from "./landing/landing_info/information"
+export {default as navigationBar} from "./nav_bar/navigation"
+export {default as menu} from "./nav_bar/menu/menu"
 export {default as discover} from "./main/main_discover/discover"
 export {default as welcome} from "./main/main_welcome/welcome"
-
-export const direccionExport = () =>  {}
-
-export { inputsName } from "./input_username/input"
-export { button } from "./button_continue/continue" 
-export { inputs } from "./inputs/inputs"
-export { Welcome } from "./welcome/welcome"
-
-
-export { app_singup } from "../screens/sign_up/sign_up" 
-export { sing_in } from "../screens/sign_in/sing_In"
-export { nav } from "./search_bar/bar"
-
-export { porfile } from "../screens/profile/profile"
-export { bar_library } from "./favorites_bar/favorites_bar"
-export { photo } from "./photo/photo"
-export { library } from "./favorites/favorite"
-
-export { main } from "../screens/main/main"
-export {landing} from "../screens/landing/landing"
+export {default as mainFind} from "./main/main_find/find"
