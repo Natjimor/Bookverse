b1705d8fe4b392625b464658a7cc365042d14fca
valen-web <128190072+valen-web@users.noreply.github.com>
2023-11-03 11:01:47 -0500

    Add files via upload

--- src/export.ts
+++ src/export.ts
@@ -1,6 +1,16 @@
 export const direccionExport = () =>  {}
 
-export { inputsName } from "./screnns/sign_up/components/input"
-export { button } from "./screnns/sign_in/components/continue" 
-export { inputs } from "./screnns/sign_in/components/inputs"
-export { Welcome } from "./screnns/sign_in/components/welcome"
+export { inputsName } from "./components/input_username/input"
+export { button } from "./components/button_continue/continue" 
+export { inputs } from "./components/inputs/inputs"
+export { Welcome } from "./components/welcome/welcome"
+
+
+export { app_singup } from "./screnns/sign_up/sign_up"
+export { sing_in } from "./screnns/sign_in/sign_in"
+export { nav } from "./components/search_bar/bar"
+
+export { porfile } from "./screnns/profile/profile"
+export { bar_library } from "./components/favorites_bar/favorites_bar"
+export { photo } from "./components/photo/photo"
+export { library } from "./components/favorites/favorite"
