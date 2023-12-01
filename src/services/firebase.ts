df531f5af06e9544636a9c5643398384060d6afe
valen-web <128190072+valen-web@users.noreply.github.com>
2023-11-30 23:59:18 -0500

    Add files via upload

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
-import { collection, addDoc } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { collection, addDoc, getDoc, getDocs, setDoc, doc } from "firebase/firestore";
 import { inputs, inputsName, button } from '../components/export';  
 
 
@@ -16,29 +17,82 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth();
+
+// export const saveUser = async (name: string, email: string, password: string) => {
+//   try {
+//     // Crear usuario en Firebase Authentication
+//     const userCredential = await createUserWithEmailAndPassword(auth, email, password); 
+//     // Obtener el usuario creado
+//     const user = userCredential.user;
+//     await setDoc(doc(db, "users", user.uid), {
+//       name: name,
+//       email: email,
+//     });
+//     console.log('Usuario creado exitosamente:', user);
+
+//   } catch (error) {
+//     console.error('Error al crear el usuario:', error);
+//   }
+// };
 
 
+//  export const saveUser = async (name: string, email: string, password: string) => {
+//   await setDoc(doc(db, "users", name), {
+//     name: name,
+//      email: email,
+//      password: password
+//   });
+// }
 
-export const user = async (val_name = "") => {
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    name: val_name,
-    email: val_name,
-    userName: val_name,
+export const saveUser = async (name: any, email: any, password: any) => {
+  const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    setDoc(doc(db, "users", name), {
+         name: name,
+          email: email,
+           
+       });
+  })
+  .catch((error) => {
+    console.error('Error al crear usuario:', error.code, error.message);
+    
   });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}}
+}
 
-const Addemail = document.querySelector(".emailInput") as HTMLInputElement
- const btn = document.querySelector(".btnContinue")
+export default{
+  saveUser
+}
 
-btn?.addEventListener("click", () =>{ 
-  let email = Addemail.value
-  user(email)
-})
 
-export default firebaseConfig
+// const addUsers = async (user: any) => {
+//   try {
+//     const userData = collection(db, "users");
+//     await addDoc(userData, user);
+//     console.log("Usuario regustado exitosamente");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
+//  const getUser = async () => {
+//   const querySnapshot = await getDocs(collection(db, "users"));
+//   const userSave: any = [];
+
+//   querySnapshot.forEach((doc) => {
+//     const user = doc.data();
+//     userSave.push({
+//       id: doc.id,
+//       ...user,
+//     });
+//   });
+//   return userSave;
+// };
+
+// export default {
+//   addUsers,
+//   getUser,
+// };
  
