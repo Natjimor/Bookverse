5404f8bfb7185dfb17ca554f6f5a979076244167
valen-web <128190072+valen-web@users.noreply.github.com>
2023-11-30 23:59:54 -0500

    Add files via upload

--- src/screens/sign_up/sign_up.ts
+++ src/screens/sign_up/sign_up.ts
@@ -1,7 +1,10 @@
-import { inputsName } from "../../components/export";
+import { addObserver, appState, dispatch } from "../../store";
+import { navigate } from "../../store/acctions";
+import { Screens } from "../../types/navigation"; 
+import { inputs, Welcome} from "../../components/export";
 import styles from "./styles.css"
 import { loadCss } from "../../utils/styles";
-console.log(inputsName);
+console.log(inputs, Welcome);
 
 
 export class app_singup extends HTMLElement {
@@ -16,16 +19,51 @@ export class app_singup extends HTMLElement {
 
   render() {
     if (this.shadowRoot) {
-      this.shadowRoot.innerHTML = `
-      <section>
-      <div id="back">
-      <div id="white">
-      <input-container></input-container>
-      <p class="account"> If ypu already have an account <a>Sing in</a></p>
-      </div>
-      </div>
-      </section>
-      `
+
+    const welcome = this.ownerDocument.createElement(`welcome-container`)
+
+    const section = this.ownerDocument.createElement("section")
+    const div =  this.ownerDocument.createElement("div")
+    div.classList.add("back")
+
+    const whiteDiv = this.ownerDocument.createElement("div")
+    whiteDiv.classList.add("white")
+
+    const inputs = this.ownerDocument.createElement(`inputs-container`)
+
+    const divP =  this.ownerDocument.createElement("div")
+    divP.classList.add("link")
+    const p = this.ownerDocument.createElement("p")
+    p.textContent = `If you already have an account`
+    p.classList.add("account")
+
+    const a = this.ownerDocument.createElement("a")
+    a.textContent = `Sing in`
+    a.classList.add("account")
+    a.addEventListener(('click'), () =>{
+        dispatch(navigate(Screens.sing_in))
+       })
+
+    whiteDiv.appendChild(welcome)
+    whiteDiv.appendChild(inputs)
+    divP.appendChild(p)
+    divP.appendChild(a)
+    whiteDiv.appendChild(divP)
+    div.appendChild(whiteDiv)
+    section.appendChild(div)
+    this.shadowRoot.appendChild(section)
+    
+
+      // this.shadowRoot.innerHTML = `
+      // <section>
+      // <div id="back">
+      // <div id="white">
+      // <input-container></input-container>
+      // <p class="account"> If ypu already have an account <a>Sing in</a></p>
+      // </div>
+      // </div>
+      // </section>
+      // `
       loadCss(this, styles);
     }
   }
