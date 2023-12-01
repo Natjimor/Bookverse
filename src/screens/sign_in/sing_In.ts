b4a3f54441a7a0f2eda8ebc202e6e40b28801cb3
valen-web <128190072+valen-web@users.noreply.github.com>
2023-12-01 10:02:20 -0500

    Add files via upload

--- src/screens/sign_in/sing_In.ts
+++ src/screens/sign_in/sing_In.ts
@@ -1,9 +1,10 @@
 
-import { inputs, welcome } from "../../components/export";
-import { button } from "../../components/button_continue/continue";
+import { addObserver, appState, dispatch } from "../../store";
+import { navigate } from "../../store/acctions";
+import { Screens } from "../../types/navigation"; 
+import  "../../components/export";
 import styles from "./styles.css"
 import { loadCss } from "../../utils/styles";
-console.log(inputs, welcome, button);
 console.log("help");
 
 
@@ -19,18 +20,40 @@ export class sing_in extends HTMLElement {
 
   render() {
     if (this.shadowRoot) {
-      this.shadowRoot.innerHTML = `
-        <section>
-        <div id="back">
-        <div id="white">
-        <welcome-container></welcome-container>
-        <inputs-container></inputs-container>
-        <button-container></button-container>
-        </div>
-        </div>
-        </section>
-        `
-        loadCss(this, styles);
+
+    const welcome = this.ownerDocument.createElement(`welcome-container`)
+
+    const section = this.ownerDocument.createElement("section")
+    const div =  this.ownerDocument.createElement("div")
+    div.classList.add("back")
+
+    const whiteDiv = this.ownerDocument.createElement("div")
+    whiteDiv.classList.add("white")
+
+    const inputs = this.ownerDocument.createElement(`sing-inputs`)
+
+    const divP =  this.ownerDocument.createElement("div")
+    divP.classList.add("link")
+    const p = this.ownerDocument.createElement("p")
+    p.textContent = `If you already have an account`
+    p.classList.add("account")
+
+    const a = this.ownerDocument.createElement("a")
+    a.textContent = `Sing up`
+    a.classList.add("account")
+    a.addEventListener(('click'), () =>{
+       dispatch(navigate(Screens.sing_up))
+       })
+
+    whiteDiv.appendChild(welcome)
+    whiteDiv.appendChild(inputs)
+    divP.appendChild(p)
+    divP.appendChild(a)
+    whiteDiv.appendChild(divP)
+    div.appendChild(whiteDiv)
+    section.appendChild(div)
+    this.shadowRoot.appendChild(section)
+    loadCss(this, styles);
     }
   }
 }
